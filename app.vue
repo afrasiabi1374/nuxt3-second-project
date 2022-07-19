1acfa1cc6e8b1e96a49af0147dee56a41f2e2c07
ali <mohammad.aligiga@gmail.com>
2022-07-19 11:56:00 +0430

    a

---
+++ app.vue
@@ -0,0 +1,133 @@
+<script setup lang="ts">
+  import { Gender, Popularity, Length, names } from "@/data"
+
+  interface OptionsState {
+    gender: Gender;
+    popularity: Popularity;
+    length: Length;
+  }
+
+  const options = reactive<OptionsState>({
+    gender : Gender.GIRL,
+    length: Length.SHORT,
+    popularity: Popularity.TRENDY,
+  })
+  const computeSelectedNames = () => {
+    const filterNames = names.filter((name)=> name.gender === options.gender)
+    .filter((name)=> name.popularity === options.popularity)
+    .filter((name) => {
+      if (options.length === Length.ALL) return true
+      else return name.length === options.length
+    })
+
+    selectedNames.value = filterNames.map(name => name.name)
+
+  }
+  const selectedNames = ref<string[]>([])
+</script>
+<template>
+  <div class="container">
+    <h1>Baby name generator</h1>
+    <p>choose your options and click find name blow </p>
+    <div class="options-container">
+      <div class="option-buttons">
+        <h4> 1) choose a gender </h4>
+        <div class="option-buttons">
+          <button class="option option-left"
+          :class="options.gender === Gender.BOY && 'option-active'"
+          @click="options.gender = Gender.BOY"
+          >Boy</button>
+          <button class="option"
+          :class="options.gender === Gender.UNISEX && 'option-active'"
+          @click="options.gender = Gender.UNISEX"
+          >Unisex</button>
+          <button class="option option-right" 
+          :class="options.gender === Gender.GIRL && 'option-active'"
+          @click="options.gender = Gender.GIRL"
+          >Girl</button>
+        </div>
+      </div>
+      <div class="option-container option-left">
+        <div class="option-buttons option-right">
+          <h4> 2) choose name's popularity </h4>
+          <div class="option-buttons">
+            <button class="option" :class="options.popularity === Popularity.TRENDY && 'option-active'" @click="options.popularity = Popularity.TRENDY">Trendy</button>
+            <button class="option" :class="options.popularity === Popularity.UNIQUE && 'option-active'" @click="options.popularity = Popularity.UNIQUE">Unique</button>
+          </div>
+        </div>
+      </div>
+      <div class="option-container">
+        <div class="option-buttons">
+          <h4> 3) choose names length </h4>
+          <div class="option-buttons">
+            <button class="option option-left" :class="options.length === Length.LONG && 'option-active'" @click="options.length = Length.LONG">Long</button>
+            <button class="option" :class="options.length === Length.ALL && 'option-active'" @click="options.length = Length.ALL">All</button>
+            <button class="option option-right" :class="options.length === Length.SHORT && 'option-active'" @click="options.length = Length.SHORT">Short</button>
+          </div>
+        </div>
+      </div>
+      <button class="primary" @click="computeSelectedNames">Find Names</button>
+    </div>
+    {{ selectedNames }}
+  </div>
+</template>
+<style>
+  .container {
+    font-family: Arial, Helvetica, sans-serif;
+    color: rgb(27, 60, 138);
+    max-width: 50rem;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+  .options-container {
+    background-color: rgb(255, 238, 236);
+    border-radius: 2rem;
+    padding: 1rem;
+    width: 95%;
+    margin: 0 auto;
+    margin-top: 4rem;
+    position: relative;
+  }
+
+  .option-container {
+    margin-bottom: 2rem;
+  }
+
+  .option {
+    background-color: white;
+    outline: 0.15rem solid rgba(249, 87, 89);
+    border: none;
+    padding: .75rem;
+    width: 12rem;
+    font-size: 1rem;
+    color: rgb(27, 60, 138);
+    cursor: pointer;
+    font-weight: 200;
+  }
+
+  .option-left {
+    border-radius: 1rem 0 0  1rem;
+  }
+
+  .option-right {
+    border-radius: 0  1rem  1rem  0;
+  }
+  .option-active {
+    background-color: rgb(249, 87, 89);
+    color: white;
+  }
+  .primary {
+    background-color: rgb(249, 87, 89);
+    color: white;
+    border-radius: 6.5rem;
+    border: none;
+    padding: 0.75rem 4rem;
+    font-size: 1rem;
+    margin-top: 1rem;
+    cursor: pointer;
+  }
+</style>
