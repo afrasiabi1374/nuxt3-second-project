0a2161cd8d7f7c03d5888a23cc2145c16fa17267
afrasiabi1374 <mohammad.aligiga@gmail.com>
2022-07-20 09:22:48 +0430

    a

--- app.vue
+++ app.vue
@@ -24,50 +24,44 @@
 
   }
   const selectedNames = ref<string[]>([])
+  const optionsArray = [
+    {
+      title: '1) choose a gender',
+      category: 'gender',
+      buttons: [Gender.GIRL, Gender.UNISEX, Gender.BOY],
+    },
+    {
+      title: '2) choose a names popularity',
+      category: 'popularity',
+      buttons: [Popularity.TRENDY, Popularity.UNIQUE],
+    },
+    {
+      title: '3) choose a names length',
+      category: 'length',
+      buttons: [Length.SHORT, Length.ALL, Length.LONG],
+    },
+  ]
 </script>
 <template>
   <div class="container">
     <h1>Baby name generator</h1>
     <p>choose your options and click find name blow </p>
     <div class="options-container">
-      <div class="option-buttons">
-        <h4> 1) choose a gender </h4>
-        <div class="option-buttons">
-          <button class="option option-left"
-          :class="options.gender === Gender.BOY && 'option-active'"
-          @click="options.gender = Gender.BOY"
-          >Boy</button>
-          <button class="option"
-          :class="options.gender === Gender.UNISEX && 'option-active'"
-          @click="options.gender = Gender.UNISEX"
-          >Unisex</button>
-          <button class="option option-right" 
-          :class="options.gender === Gender.GIRL && 'option-active'"
-          @click="options.gender = Gender.GIRL"
-          >Girl</button>
-        </div>
-      </div>
-      <div class="option-container option-left">
-        <div class="option-buttons option-right">
-          <h4> 2) choose name's popularity </h4>
-          <div class="option-buttons">
-            <button class="option" :class="options.popularity === Popularity.TRENDY && 'option-active'" @click="options.popularity = Popularity.TRENDY">Trendy</button>
-            <button class="option" :class="options.popularity === Popularity.UNIQUE && 'option-active'" @click="options.popularity = Popularity.UNIQUE">Unique</button>
-          </div>
-        </div>
-      </div>
-      <div class="option-container">
-        <div class="option-buttons">
-          <h4> 3) choose names length </h4>
-          <div class="option-buttons">
-            <button class="option option-left" :class="options.length === Length.LONG && 'option-active'" @click="options.length = Length.LONG">Long</button>
-            <button class="option" :class="options.length === Length.ALL && 'option-active'" @click="options.length = Length.ALL">All</button>
-            <button class="option option-right" :class="options.length === Length.SHORT && 'option-active'" @click="options.length = Length.SHORT">Short</button>
-          </div>
-        </div>
-      </div>
-      <button class="primary" @click="computeSelectedNames">Find Names</button>
+    <Option
+      v-for="option in optionsArray"
+      :key="option.title"
+      :option="option"
+      :options="options"
+    />
+    <button class="primary" @click="computeSelectedNames">Find Names</button>
     </div>
+    <div class="cards-container">
+      <div v-for="name in selectedNames" :key="name" class="card" >
+        <h4>{{name}}</h4>
+        <p>x</p>
+      </div>
+    </div>
+
     {{ selectedNames }}
   </div>
 </template>
@@ -79,47 +73,19 @@
     margin: 0 auto;
     text-align: center;
   }
-
-  h1 {
-    font-size: 3rem;
-  }
   .options-container {
     background-color: rgb(255, 238, 236);
     border-radius: 2rem;
-    padding: 1rem;
+    padding: .1rem;
     width: 95%;
     margin: 0 auto;
     margin-top: 4rem;
     position: relative;
   }
-
-  .option-container {
-    margin-bottom: 2rem;
+  h1 {
+    font-size: 3rem;
   }
-
-  .option {
-    background-color: white;
-    outline: 0.15rem solid rgba(249, 87, 89);
-    border: none;
-    padding: .75rem;
-    width: 12rem;
-    font-size: 1rem;
-    color: rgb(27, 60, 138);
-    cursor: pointer;
-    font-weight: 200;
-  }
-
-  .option-left {
-    border-radius: 1rem 0 0  1rem;
-  }
-
-  .option-right {
-    border-radius: 0  1rem  1rem  0;
-  }
-  .option-active {
-    background-color: rgb(249, 87, 89);
-    color: white;
-  }
+ 
   .primary {
     background-color: rgb(249, 87, 89);
     color: white;
@@ -130,4 +96,30 @@
     margin-top: 1rem;
     cursor: pointer;
   }
+
+  .cards-container {
+    display: flex;
+    margin-top: 3rem;
+    flex-wrap: wrap;
+  }
+
+  .card {
+    background-color: rgb(27, 60, 138);
+    width: 28%;
+    color: white;
+    border-radius: 1rem;
+    padding: 1rem;
+    margin-right: .5rem;
+    margin-bottom: 1rem;
+    position: relative;
+  }
+
+  .card p {
+    position: absolute;
+    top: -30%;
+    left: 92.5%;
+    color: rgba(255, 255, 255, .178);
+  }
+  /* 205 */
 </style>
+
---
+++ components/Option.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { Gender, Popularity, Length } from '@/data';
+
+    interface OptionProps {
+        option: {
+            title: string;
+            category: string;
+            buttons: Gender[] | Popularity[] | Length[];
+        };
+        options: {
+            gender: Gender;
+            popularity: Popularity;
+            length: Length;
+        }
+
+    }
+    const props = defineProps<OptionProps>()
+</script>
+<template>
+    <div class="option-buttons">
+    <h4>{{option.title}}</h4>
+        <button
+            v-for="value in option.buttons"
+            :key="value"
+            class="option option-left"
+            :class="options[option.category] === value && 'option-active'"
+            @click="options[option.category] = value"
+        >{{value}}</button>
+    </div>
+</template>
+<style scoped>
+
+
+  .option-container {
+    margin-bottom: 2rem;
+  }
+
+  .option {
+    background-color: white;
+    outline: 0.15rem solid rgba(249, 87, 89);
+    border: none;
+    padding: .75rem;
+    width: 12rem;
+    font-size: 1rem;
+    color: rgb(27, 60, 138);
+    cursor: pointer;
+    font-weight: 200;
+  }
+
+  .option-left {
+    border-radius: 1rem 0 0  1rem;
+  }
+
+  .option-right {
+    border-radius: 0  1rem  1rem  0;
+  }
+  .option-active {
+    background-color: rgb(249, 87, 89);
+    color: white;
+  }
+</style>
