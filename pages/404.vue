3d20fd6dac10aaa94835fddd13026e25dbcf454c
ali <mohammad.aligiga@gmail.com>
2022-07-24 13:22:19 +0430

    a

---
+++ pages/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>
+        Errorr 404 سفارشی
+    </h1>
+    <NuxtLink to="/restaurants">Go Back</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/restaurants/[name].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import {} from "#app"
+    import restaurants from "@/data.json"
+    const route = useRoute()
+    const name = route.params.name
+
+    const restaurant = restaurants.find(r => r.name === name)
+</script>
+
+<template>
+    <div>
+        {{ restaurant }}
+    </div>
+</template>
